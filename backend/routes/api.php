68f42911153befbb56f6e0343d74868a9f000bb9
MrKabado <jersonjaybonghanoy@gmail.com>
2026-05-22 19:55:19 +0800

    Chore: added routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,14 +1,46 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\GradeController;
+use App\Http\Controllers\GradeReportController;
+use App\Http\Controllers\SectionController;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\StudentReportController;
+use App\Http\Controllers\SubjectController;
 
 Route::prefix('auth')->group(function () {
+
     Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::get('/user', [AuthController::class, 'user']);
+    });
+});
+
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+
+    Route::apiResource('students', StudentController::class);
+
+    Route::apiResource('sections', SectionController::class)
+        ->except(['show']);
+
+    Route::apiResource('subjects', SubjectController::class)
+        ->except(['show']);
+    Route::apiResource('grades', GradeController::class)
+        ->parameters([
+            'grades' => 'student',
+        ]);
+
+    Route::get('/grade-reports', [GradeReportController::class, 'index']);
+
+    Route::get('/grade-reports/{student}/pdf', [StudentReportController::class, 'pdfDownload']);
+
+    Route::post('/grade-reports/{student}/approve', [StudentReportController::class, 'approve']);
 });
